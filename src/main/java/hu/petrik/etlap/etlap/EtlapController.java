c20993c6cec833652978a773c5bd93e6506d6476
Kormos Csanad <csanadsuli19@gmail.com>
2022-12-15 11:30:43 +0100

    Étlap fxml is done Nuggets

---
+++ src/main/java/hu/petrik/etlap/etlap/EtlapController.java
@@ -0,0 +1,33 @@
+package hu.petrik.etlap.etlap;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class EtlapController {
+
+    @FXML
+    private Spinner spinnerForintEmeles;
+    @FXML
+    private ToggleButton toggleButtonAr;
+    @FXML
+    private Button buttonSzazalekEmeles;
+    @FXML
+    private ToggleButton toggleButtonName;
+    @FXML
+    private ListView listViewEtelek;
+    @FXML
+    private ToggleButton toggleButtonKategoria;
+    @FXML
+    private Spinner spinnerSzazalekEmeles;
+    @FXML
+    private Button buttonEtlapTorles;
+    @FXML
+    private Button buttonForintEmeles;
+    @FXML
+    private Button buttonEtlapUjEtel;
+
+    @Deprecated
+    protected void onHelloButtonClick() {
+
+    }
+}
